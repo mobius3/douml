2258f47a4f733dd197d4cace851337a5d6394b8f
habipyesilyurt <habip.yesilyurt@gmail.com>
2016-01-09 11:29:16 +0200

    costructor/destrucor position
    Newly created construcot/destructor moved to top of class
    #110

--- src/dialog/RelationDialog.cpp
+++ src/dialog/RelationDialog.cpp
@@ -451,7 +451,7 @@ RelationDialog::RelationDialog(RelationData * r)
     edTypeActivated(edtype->currentIndex());
 
     connect(m_tabWidget, SIGNAL(currentChanged(int)),
-            this, SLOT(update_all_tabs(int));
+            this, SLOT(update_all_tabs(int)));
 
     open_dialog(this);
 }
